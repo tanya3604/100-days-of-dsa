7e655d3fd0cbe0c009de0a146972a7444d0071be
tanya3604 <tanyakeshwani1@gmail.com>
2026-04-06 04:07:44 +0530

    Add files via upload

---
+++ day52.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0) return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = newNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n)
+                nodes[i]->left = nodes[left];
+            if (right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    return nodes[0];
+}
+
+// Find LCA
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    struct Node* left = findLCA(root->left, n1, n2);
+    struct Node* right = findLCA(root->right, n1, n2);
+
+    if (left != NULL && right != NULL)
+        return root;
+
+    return (left != NULL) ? left : right;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* root = buildTree(arr, n);
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+    else
+        printf("LCA not found\n");
+
+    return 0;
+}
