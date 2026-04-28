436aff930ad50792f54c5f96be99279b5c8379ee
tanya3604 <tanyakeshwani1@gmail.com>
2026-04-28 22:41:18 +0530

    Add files via upload

---
+++ day85.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, key;
+
+    // Input size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input elements
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort
+    for(i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Shift elements greater than key
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    // Output sorted array
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
